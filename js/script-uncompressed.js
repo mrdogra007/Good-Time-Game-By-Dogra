ff19b409e2bdfbb81bd93f853532c735c29a55b3
Nishant Dogra <13938655+mrdogra007@users.noreply.github.com>
2018-04-24 16:00:49 +0530

    24-04-2018
    Don't Waste Good Time, Checkout your image learning and remembrance power with this game. This trick will surely increase your remembrance power #DontWasteGoodTime

---
+++ js/script-uncompressed.js
@@ -0,0 +1,231 @@
+// JavaScript sheet; Ver. 2.0; Author: Nishant Dogra; Initial built: 09-12-2017; Updated: 23-04-2018
+
+var bollywood = [
+	'http://cloud.oyephoto.in/good-time-game/bollywood/amitabh-bachchan.jpg',
+	'http://cloud.oyephoto.in/good-time-game/bollywood/amrish-puri.jpg',
+	'http://cloud.oyephoto.in/good-time-game/bollywood/dev-anand.jpg',
+	'http://cloud.oyephoto.in/good-time-game/bollywood/dharmendra.jpg',
+	'http://cloud.oyephoto.in/good-time-game/bollywood/govinda.jpg',
+	'http://cloud.oyephoto.in/good-time-game/bollywood/rajinikanth.jpg',
+	'http://cloud.oyephoto.in/good-time-game/bollywood/raj-kapoor.jpg',
+	'http://cloud.oyephoto.in/good-time-game/bollywood/razak-khan.jpg',
+	'http://cloud.oyephoto.in/good-time-game/bollywood/shakti-kapoor.jpg',
+	'http://cloud.oyephoto.in/good-time-game/bollywood/sunil-dutt.jpg',
+	'http://cloud.oyephoto.in/good-time-game/bollywood/amitabh-bachchan.jpg',
+	'http://cloud.oyephoto.in/good-time-game/bollywood/amrish-puri.jpg',
+	'http://cloud.oyephoto.in/good-time-game/bollywood/dev-anand.jpg',
+	'http://cloud.oyephoto.in/good-time-game/bollywood/dharmendra.jpg',
+	'http://cloud.oyephoto.in/good-time-game/bollywood/govinda.jpg',
+	'http://cloud.oyephoto.in/good-time-game/bollywood/rajinikanth.jpg',
+	'http://cloud.oyephoto.in/good-time-game/bollywood/raj-kapoor.jpg',
+	'http://cloud.oyephoto.in/good-time-game/bollywood/razak-khan.jpg',
+	'http://cloud.oyephoto.in/good-time-game/bollywood/shakti-kapoor.jpg',
+	'http://cloud.oyephoto.in/good-time-game/bollywood/sunil-dutt.jpg'
+];
+
+var cartoon = [
+	'http://cloud.oyephoto.in/good-time-game/cartoon/boxer-motu-patlu.jpg',
+	'http://cloud.oyephoto.in/good-time-game/cartoon/chacha-chaudhary.jpg',
+	'http://cloud.oyephoto.in/good-time-game/cartoon/chhota-mannu.jpg',
+	'http://cloud.oyephoto.in/good-time-game/cartoon/chota-bheem.jpg',
+	'http://cloud.oyephoto.in/good-time-game/cartoon/chutki.jpg',
+	'http://cloud.oyephoto.in/good-time-game/cartoon/kemuzo-kemumaki.jpg',
+	'http://cloud.oyephoto.in/good-time-game/cartoon/kenichi-mitsuba.jpg',
+	'http://cloud.oyephoto.in/good-time-game/cartoon/motu-cartoon.jpg',
+	'http://cloud.oyephoto.in/good-time-game/cartoon/ninja hathodi.jpg',
+	'http://cloud.oyephoto.in/good-time-game/cartoon/patlu-cartoon.jpg',
+	'http://cloud.oyephoto.in/good-time-game/cartoon/boxer-motu-patlu.jpg',
+	'http://cloud.oyephoto.in/good-time-game/cartoon/chacha-chaudhary.jpg',
+	'http://cloud.oyephoto.in/good-time-game/cartoon/chhota-mannu.jpg',
+	'http://cloud.oyephoto.in/good-time-game/cartoon/chota-bheem.jpg',
+	'http://cloud.oyephoto.in/good-time-game/cartoon/chutki.jpg',
+	'http://cloud.oyephoto.in/good-time-game/cartoon/kemuzo-kemumaki.jpg',
+	'http://cloud.oyephoto.in/good-time-game/cartoon/kenichi-mitsuba.jpg',
+	'http://cloud.oyephoto.in/good-time-game/cartoon/motu-cartoon.jpg',
+	'http://cloud.oyephoto.in/good-time-game/cartoon/ninja hathodi.jpg',
+	'http://cloud.oyephoto.in/good-time-game/cartoon/patlu-cartoon.jpg'
+];
+
+var fruits = [
+	'http://cloud.oyephoto.in/good-time-game/fruits/apple.jpg',
+	'http://cloud.oyephoto.in/good-time-game/fruits/banana.jpg',
+	'http://cloud.oyephoto.in/good-time-game/fruits/chikoo.jpg',
+	'http://cloud.oyephoto.in/good-time-game/fruits/guava.jpg',
+	'http://cloud.oyephoto.in/good-time-game/fruits/kiwi.jpg',
+	'http://cloud.oyephoto.in/good-time-game/fruits/mango.jpg',
+	'http://cloud.oyephoto.in/good-time-game/fruits/orange.jpg',
+	'http://cloud.oyephoto.in/good-time-game/fruits/pomegranate.jpg',
+	'http://cloud.oyephoto.in/good-time-game/fruits/sitafal.jpg',
+	'http://cloud.oyephoto.in/good-time-game/fruits/straw-berry.jpg',
+	'http://cloud.oyephoto.in/good-time-game/fruits/apple.jpg',
+	'http://cloud.oyephoto.in/good-time-game/fruits/banana.jpg',
+	'http://cloud.oyephoto.in/good-time-game/fruits/chikoo.jpg',
+	'http://cloud.oyephoto.in/good-time-game/fruits/guava.jpg',
+	'http://cloud.oyephoto.in/good-time-game/fruits/kiwi.jpg',
+	'http://cloud.oyephoto.in/good-time-game/fruits/mango.jpg',
+	'http://cloud.oyephoto.in/good-time-game/fruits/orange.jpg',
+	'http://cloud.oyephoto.in/good-time-game/fruits/pomegranate.jpg',
+	'http://cloud.oyephoto.in/good-time-game/fruits/sitafal.jpg',
+	'http://cloud.oyephoto.in/good-time-game/fruits/straw-berry.jpg'
+];
+
+var patriotism = [
+	'http://cloud.oyephoto.in/good-time-game/patriotism/bal-gangadhar-tilak.jpg',
+	'http://cloud.oyephoto.in/good-time-game/patriotism/bhagat-singh.jpg',
+	'http://cloud.oyephoto.in/good-time-game/patriotism/b-r-ambedkar.jpg',
+	'http://cloud.oyephoto.in/good-time-game/patriotism/chandra-hekhar-azad.jpg',
+	'http://cloud.oyephoto.in/good-time-game/patriotism/k-b-hedgewar.jpg',
+	'http://cloud.oyephoto.in/good-time-game/patriotism/mangal-pandey.jpg',
+	'http://cloud.oyephoto.in/good-time-game/patriotism/netaji-subhash-chandra-bose.jpg',
+	'http://cloud.oyephoto.in/good-time-game/patriotism/rani-lakshmi-bai.jpg',
+	'http://cloud.oyephoto.in/good-time-game/patriotism/sardar-vallabhbhai-patel.jpg',
+	'http://cloud.oyephoto.in/good-time-game/patriotism/swami-vivekananda.jpg',
+	'http://cloud.oyephoto.in/good-time-game/patriotism/bal-gangadhar-tilak.jpg',
+	'http://cloud.oyephoto.in/good-time-game/patriotism/bhagat-singh.jpg',
+	'http://cloud.oyephoto.in/good-time-game/patriotism/b-r-ambedkar.jpg',
+	'http://cloud.oyephoto.in/good-time-game/patriotism/chandra-hekhar-azad.jpg',
+	'http://cloud.oyephoto.in/good-time-game/patriotism/k-b-hedgewar.jpg',
+	'http://cloud.oyephoto.in/good-time-game/patriotism/mangal-pandey.jpg',
+	'http://cloud.oyephoto.in/good-time-game/patriotism/netaji-subhash-chandra-bose.jpg',
+	'http://cloud.oyephoto.in/good-time-game/patriotism/rani-lakshmi-bai.jpg',
+	'http://cloud.oyephoto.in/good-time-game/patriotism/sardar-vallabhbhai-patel.jpg',
+	'http://cloud.oyephoto.in/good-time-game/patriotism/swami-vivekananda.jpg'
+];
+
+var places = [
+	'http://cloud.oyephoto.in/good-time-game/places/bekal-fort-kerala.jpg',
+	'http://cloud.oyephoto.in/good-time-game/places/cooch-behar-palace-west-bengal.jpg',
+	'http://cloud.oyephoto.in/good-time-game/places/india-gate-mumbai.jpg',
+	'http://cloud.oyephoto.in/good-time-game/places/lal-kila-delhi.jpg',
+	'http://cloud.oyephoto.in/good-time-game/places/lingaraj-temple-bhubaneswar.jpg',
+	'http://cloud.oyephoto.in/good-time-game/places/masroor-rock-cut-temple-himachal.jpg',
+	'http://cloud.oyephoto.in/good-time-game/places/mathura-vrindavan-agra.jpg',
+	'http://cloud.oyephoto.in/good-time-game/places/mehrangarh-fort-jodhpur.jpg',
+	'http://cloud.oyephoto.in/good-time-game/places/murudeshwar-karnataka.jpg',
+	'http://cloud.oyephoto.in/good-time-game/places/taj-mahal-agra.jpg',
+	'http://cloud.oyephoto.in/good-time-game/places/bekal-fort-kerala.jpg',
+	'http://cloud.oyephoto.in/good-time-game/places/cooch-behar-palace-west-bengal.jpg',
+	'http://cloud.oyephoto.in/good-time-game/places/india-gate-mumbai.jpg',
+	'http://cloud.oyephoto.in/good-time-game/places/lal-kila-delhi.jpg',
+	'http://cloud.oyephoto.in/good-time-game/places/lingaraj-temple-bhubaneswar.jpg',
+	'http://cloud.oyephoto.in/good-time-game/places/masroor-rock-cut-temple-himachal.jpg',
+	'http://cloud.oyephoto.in/good-time-game/places/mathura-vrindavan-agra.jpg',
+	'http://cloud.oyephoto.in/good-time-game/places/mehrangarh-fort-jodhpur.jpg',
+	'http://cloud.oyephoto.in/good-time-game/places/murudeshwar-karnataka.jpg',
+	'http://cloud.oyephoto.in/good-time-game/places/taj-mahal-agra.jpg'
+];
+
+var images = [],
+    theme = "",
+    count = 19,
+    rand = 0,
+    tempElt1 = "",
+    tempElt2 = "",
+    click = -1,
+    score = 0,
+    win = 0,
+    time = 0;
+
+var preElt = document.querySelector("#pre"),
+    themesElts = document.querySelectorAll(".themes"),
+    boxElts = document.querySelectorAll(".box"),
+    timeElt = document.querySelector("#time"),
+    scoreElt = document.querySelector("#score"),
+    postElt = document.querySelector("#post"),
+    finalElt = document.querySelector("#final"),
+	quizElt = document.querySelector(".quiz-grid"),
+	caseFirstMsgElt = document.querySelector(".case-first"),
+	caseSecondMsgElt = document.querySelector(".case-second");
+
+// initiate the game with chosen theme
+for (let j=0; j<themesElts.length; j++) {
+  themesElts[j].addEventListener("click", function () {
+    theme = this.id;
+    // insert theme in images array
+    switch (theme) {
+      case "bollywood":
+        for (let i=0; i<20; i++) {images.push(bollywood[i]);}
+        break;
+      case "cartoon":
+        for (let i=0; i<20; i++) {images.push(cartoon[i]);}
+        break;
+      case "fruits":
+        for (let i=0; i<20; i++) {images.push(fruits[i]);}
+        break;
+      case "patriotism":
+        for (let i=0; i<20; i++) {images.push(patriotism[i]);}
+        break;
+      case "places":
+        for (let i=0; i<20; i++) {images.push(places[i]);}
+        break;
+    }
+
+    // insert images in memory game
+    for (let i=0; i<20; i++) {
+      rand = Math.floor(Math.random() * count);
+      boxElts[i].innerHTML = "<img src='" + images[rand] + "' alt='image' class='hidden'>";
+      images.splice(rand, 1);
+      count--;
+    }
+    preElt.style.display = "none";
+    caseFirstMsgElt.style.display = "none";
+    caseSecondMsgElt.style.display = "block";
+    quizElt.style.display = "block";
+  });
+}
+
+// Handle the play
+for (let j=0; j<document.querySelectorAll(".play").length; j++) {
+  document.querySelectorAll(".play")[j].addEventListener("click", function () {
+    this.firstChild.classList.remove("hidden");
+
+    // first of two click
+    if (click < 1) {
+      tempElt1 = this;
+      // timer
+      if (click === -1) {
+        timer = setInterval(function() {
+          time++;
+          timeElt.innerHTML = "Time taken: " + time + " sec";
+        }, 1000);
+      }
+      click = 1;
+    }
+
+    // second click
+    else if (this !== tempElt1) {
+      tempElt2 = this;
+
+      // case to run when different selection
+      if (tempElt1.firstChild.src !== tempElt2.firstChild.src) {
+        setTimeout( function() {
+          tempElt1.firstChild.classList.add("hidden");
+          tempElt2.firstChild.classList.add("hidden");
+        }, 400);
+        if (score > 0){
+          score -= 2;
+        }
+        scoreElt.innerHTML = "Score card: " + score;
+      }
+
+      // case to run when same selection
+      else {
+        score += 10;
+        win += 2;
+        tempElt1.firstChild.classList.add("outlined");
+        tempElt2.firstChild.classList.add("outlined");
+        tempElt1.classList.remove("play");
+        tempElt2.classList.remove("play");
+        scoreElt.innerHTML = "Score card: " + score;
+
+        // result if game won
+        if (win === 20) {
+          clearTimeout(timer);
+          finalElt.innerHTML = "You won " + score + " points <br> in " + time + " seconds";
+          postElt.style.display = "flex";
+        }
+      }
+      click = 0;
+    }
+  });
+}
